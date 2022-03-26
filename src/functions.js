975b2648068efc0d30c022b0e9f77bafe6fdfa7d
Alfat1989 <gilmalf89@gmail.com>
2022-03-26 19:32:02 +0300

    переносит функцию в основной файл

--- src/functions.js
+++
@@ -1,4 +0,0 @@
-export function timeValue(data) {
-    localStorage.setItem(PLATER_TIME, JSON.stringify(data))
-    localStorage.setItem('hello')
-}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import throttle from "lodash.throttle";
-import {timeValue} from "./functions"
 
 
 const iframe = document.querySelector('iframe')
@@ -11,6 +10,11 @@ console.log(player)
 const PLATER_TIME='videoplayer-current-time'
 
 player.on('timeupdate', throttle(timeValue, 1000))
+
+function timeValue(data) {
+    localStorage.setItem(PLATER_TIME, JSON.stringify(data))
+    
+}
     
 
 const getValue = localStorage.getItem(PLATER_TIME)
@@ -21,39 +25,39 @@ player.setCurrentTime(getValuePars.seconds)
 
 ////////////////////////////////////////////////
 
-// const form = document.querySelector('form')
-// const emailInput=document.querySelector('input')
-// const textarea = document.querySelector('textarea')
+const form = document.querySelector('form')
+const emailInput=document.querySelector('input')
+const textarea = document.querySelector('textarea')
 
 
-// form.addEventListener('submit', onFormSubmit)
-// form.addEventListener('input', throttle(onFormInput, 1000))
+form.addEventListener('submit', onFormSubmit)
+form.addEventListener('input', throttle(onFormInput, 1000))
 
 
-// function onFormInput(e) {
+function onFormInput(e) {
   
-//     localStorage.setItem(e.target.name, e.target.value)
+    localStorage.setItem(e.target.name, e.target.value)
 
-// }
+}
 
-// function populateText() {
-//     const savedEmail = localStorage.getItem('email')
-//     const savedMes = localStorage.getItem('message')
-//     if (savedEmail||savedMes) {
-//         form.elements.email.value=savedEmail
-//         form.elements.message.value=savedMes
-//     }
+function populateText() {
+    const savedEmail = localStorage.getItem('email')
+    const savedMes = localStorage.getItem('message')
+    if (savedEmail||savedMes) {
+        form.elements.email.value=savedEmail
+        form.elements.message.value=savedMes
+    }
     
-// }
+}
 
-// populateText()
+populateText()
 
-// function onFormSubmit(e) {
-//     e.preventDefault()
-//     e.currentTarget.reset()
-//     console.log('Pushed')
-//     localStorage.removeItem('email')
-//     localStorage.removeItem('message')
+function onFormSubmit(e) {
+    e.preventDefault()
+    e.currentTarget.reset()
+    console.log('Pushed')
+    localStorage.removeItem('email')
+    localStorage.removeItem('message')
     
-// }
+}
 
